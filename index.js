35c5454e0b15b565751dce59b20ba9121ce32798
RichardLee1978 <shlrlj@163.com>
2016-07-08 11:25:16 +0800

    added index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+var through = require('through2');
+var gutil = require('gulp-util');
+var path = require('path');
+
+module.exports = function(name, prefix, rename) {
+	console.log('..............task runing................')
+	var fileList = [];
+	return through.obj(function (file, enc, cb) {
+		if (file.isDirectory()) {
+			return cb(null, file);
+		}
+		if (file.isStream()) {
+			msg = 'Streaming not supported';
+			return cb(new gutil.PluginError(pluginName), msg);
+		}
+		var relative = path.relative(file.cwd, file.path);
+		var extension = path.extname(relative);
+		var filename= path.basename(relative).slice(0, -extension.length);
+		var filePath = {};
+			filePath[filename] = {}
+		//添加时间戳
+		filePath[filename][relative] = now.toString();
+		fileList.push(filePath);
+		cb();
+	}, function (cb) {
+
+		var fileListFile = new File({
+			path: name,
+			contents: new Buffer(JSON.stringify(fileList, null, '  '))
+		});
+		var name_ = path.join(__dirname, name);
+
+		this.push(new gutil.File({
+			path: name_,
+			contents: new Buffer(JSON.stringify(fileList, null, '  '))
+		}));
+		console.log('..............task end................')
+		cb();
+	});
+}
